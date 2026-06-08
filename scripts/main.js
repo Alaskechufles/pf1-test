a868b75fb1a53d92d81173688cc293e44066e8d1
Diego <diegohuarsayacruz@gmail.com>
2026-06-08 13:51:54 -0500

    Add image loading functionality and update HTML structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
 let lugares = document.querySelector("#lugares")
+
+async function traerDatos() {
+    try {
+        let response = await fetch("assets/stays.json")
+        let data = await response.json()
+        return data
+    } catch (error) {
+        console.error("ups", error)
+    }
+}
+
+let datosLugares = await traerDatos()
+
+console.log(datosLugares)
+
+for (let i = 0; i < datosLugares.length; i++) {
+    const element = datosLugares[i];
+    lugares.innerHTML += `
+    <img src=${element.photo} alt="" width="400">
+    `
+}
